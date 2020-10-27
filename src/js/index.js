e9ceb190b7e464afd7b4314f134c4a4c9bf002c6
Arxont <hanterokun@gmail.com>
2020-10-27 16:53:27 +0200

    Search functionality implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,26 @@
-// Global app controller
-const x = 53;
-let z = 13;
-let q = x + z;
-console.log(q)
-console.log(`wedfg + ${q}`);
+import Search from './models/Search';
+import * as searchView from './views/searchViews';
+import {elements, renderLoader, clearLoader} from './views/base';
+
+const state ={};
+const controlSearch = async () => {
+const query =  searchView.getInput();
+
+if(query){
+    state.search = new Search(query);
+
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.seachRes);
+
+
+    await state.search.getResults();
+
+    clearLoader();
+    searchView.renderResults(state.search.result);
+}
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
